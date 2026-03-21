013df4a1b4e8a47a40da2041b3f7a5f148ec471a
Bala <baladede@gmail.com>
2026-03-21 14:38:56 +0530

    Initial Commit

---
+++ MacDroidControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacDroidControl
+//
+//  Created by Dante on 21/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacDroidControl/MacDroidControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacDroidControlApp.swift
+//  MacDroidControl
+//
+//  Created by Dante on 21/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MacDroidControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
